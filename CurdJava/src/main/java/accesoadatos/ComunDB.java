cd281e74be99786b85940e465f631e41fec2e2c1
Ángel Miguel Mena Argumedo <angelmeba4@gmail.com>
2024-06-05 19:09:08 -0600

    Creacion de la clases

--- CurdJava/src/main/java/accesoadatos/ComunDB.java
+++ CurdJava/src/main/java/accesoadatos/ComunDB.java
@@ -3,11 +3,25 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package accesoadatos;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 /**
  *
  * @author MINEDUCYT
  */
 public class ComunDB {
-    
+     static String connectionUrl = "jdbc:sqlserver://COMPU:1433;"
+            + "databaseName=Practica20240604DB;"
+            + "user=java0605;"
+            + "password=12345;"
+            + "loginTimeout=30;encrypt=false;trustServerCertificate=false";
+     public static Connection obtenerConexion() throws SQLException {
+        // Registrar el Driver de la conexion a la base de datos SQL server
+        // para que lo reconozca el servidor web
+        DriverManager.registerDriver(new com.microsoft.sqlserver.jdbc.SQLServerDriver());
+        Connection connection = DriverManager.getConnection(connectionUrl); // abrir la conexion a la base de datos
+        return connection; // retornar la conexion a la base de datos
+    }
 }
