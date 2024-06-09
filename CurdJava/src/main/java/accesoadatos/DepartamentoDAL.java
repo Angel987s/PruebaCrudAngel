bb297496da532536fa0b337c4c6eddd45bfc2c8c
Ángel Miguel Mena Argumedo <angelmeba4@gmail.com>
2024-06-08 22:05:57 -0600

    agregacion de cambios en la clase departamento dal

--- CurdJava/src/main/java/accesoadatos/DepartamentoDAL.java
+++ CurdJava/src/main/java/accesoadatos/DepartamentoDAL.java
@@ -38,4 +38,63 @@ public class DepartamentoDAL {
         }
         return departamentos;
     }
+    public static int modificar(Departamento departamento) {
+        try (Connection conn = ComunDB.obtenerConexion()) {
+
+            String sql = "UPDATE Departamentos SET Nombre=?, Descripcion=?";
+            try (PreparedStatement statement = conn.prepareStatement(sql)) {
+                statement.setString(1, departamento.getNombre());
+                statement.setString(2, departamento.getDescripcion());
+                int rowsAffected = statement.executeUpdate();
+                return rowsAffected;
+            } catch (SQLException e) {
+                throw new RuntimeException("Error al crear el producto", e);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Error al obtener la conexión a la base de datos", e);
+        }
+    }
+    
+    public static int eliminar(Departamento departamento) {
+        try (Connection conn = ComunDB.obtenerConexion()) {
+
+            String sql = "DELETE FROM Empleados WHERE EmpleadoID=?";
+            try (PreparedStatement statement = conn.prepareStatement(sql)) {
+                statement.setInt(1, departamento.getDepatamentoId());
+                int rowsAffected = statement.executeUpdate();
+                return rowsAffected;
+            } catch (SQLException e) {
+                throw new RuntimeException("Error al crear el producto", e);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Error al obtener la conexión a la base de datos", e);
+        }
+    }
+    
+    public static ArrayList<Departamento> buscar(Departamento empleadoSearch) {
+        ArrayList<Departamento> empleados = new ArrayList<>();
+        try (Connection conn = ComunDB.obtenerConexion()) {
+            String sql = "SELECT Nombre , Descripcion FROM Departamentos";
+            sql+=" WHERE Nombre LIKE ? ";
+            try (PreparedStatement statement = conn.prepareStatement(sql)) {
+                statement.setString(1, "%" + empleadoSearch.getNombre() + "%");
+                try (ResultSet resultSet = statement.executeQuery()) {
+                    while (resultSet.next()) {
+                        Departamento empleado = new Departamento();
+                        empleado.setDepartamentoId(resultSet.getInt("EmpleadoID"));
+                        empleado.setNombre(resultSet.getString("Nombre"));
+                        empleado.setDescripcion(resultSet.getString("Apellido"));
+                        
+                    }
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException("Error al buscar productos", e);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Error al obtener la conexión a la base de datos", e);
+        }
+        return empleados;
+    }
+    
+    
 }
